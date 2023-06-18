f7ddfa707e1e7feef6d571a6142fd1adaf06edf7
watanabe-himajin <c612201036@tokushima-u.ac.jp>
2023-06-18 21:59:08 +0900

    PlayerMove実装
    実装しました

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    private int a = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        a = a + 10;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(a == 1)
+        {
+            Debug.Log("a = 1Ç≈Ç∑");
+            a = a + 9;
+        }
+        else
+        {
+            Debug.Log("aÇ1Ç…ÇµÇ‹Ç∑ÅB");
+            a--;
+        }
     }
 }
